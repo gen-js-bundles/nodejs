4498866179fb48659b6a62cac50fc10056cf0027
ludo1026 <ludo1026@free.fr>
2015-03-03 11:31:00 +0100

    nodejs

--- tasks/start.js
+++ tasks/start.js
@@ -2,34 +2,18 @@ var
   inquirer = require("inquirer"),
   fs = require('fs'),
   path = require('path'),
-  exec = require('child_process').exec,
-  async = require('async');
+  exec = require('child_process').exec;
 
 module.exports = {
   do: function(data, callback) {
-    // Start
-    var questions = [
-      {
-        type: 'confirm',
-        name: 'start',
-        message: 'Start the application ?',
-        default: true
-      }
-    ];
-    inquirer.prompt(questions, function( answers ) {
+    // Install
+    console.log('npm install');
 
-      if(answers.start) {
-        console.log('start');
-        // console.log(data);
-        // console.log(data.Genjsfile);
-        var outPath = path.join(process.cwd(),data.Genjsfile.config.outDir);
-        var command = 'npm install';
-        console.log('=>',command,' in ',outPath);
-        var child = exec(command, {cwd: outPath});
-        child.stdout.pipe(process.stdout);
-        child.stderr.pipe(process.stderr);
-      }
-
-    });
+    var outPath = path.join(process.cwd(),data.Genjsfile.config.outDir);
+    var command = 'npm install';
+    console.log('=>',command,' in ',outPath);
+    var child = exec(command, {cwd: outPath});
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stderr);
   }
 };
